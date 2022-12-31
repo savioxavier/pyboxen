41111e503ee7b95b33544037ad5250a81f4b0273
Skyascii <savioxavier112@gmail.com>
2022-12-31 22:23:16 +0400

    release: 1.1.1

--- pyboxen/__init__.py
+++ pyboxen/__init__.py
@@ -4,6 +4,6 @@ Package boxen allows you to create customizable boxes in the terminal
 It is built on top of Rich, and is fully compatible with most Rich styling syntax and Rich renderables
 """
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 from .main import *
